754fbacdb0b10cbe90798b692dc43ad22aa16be4
splicid <luis1abreu11@gmail.com>
2022-11-30 00:53:04 -0500

    Fixed some issues

--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -3,10 +3,20 @@
 
 const experience = () => {
     return(
-        <div className="flex items-center justify-center h-[100vh] bg-gradient-to-r from-cyan-300 to-blue-500">
-            <div class="grid grid-cols-2 gap-4 w-screen">
-                <div className="text-center border-solid border-2 border-indigo-900 px-10 py-10 bg-white max-w-7xl ml-16">01</div>
-                <div className="text-center border-solid border-2 border-indigo-900 px-10 py-10 bg-white max-w-7xl mr-16">02</div>
+        <div className="flex items-center justify-center h-[100vh] bg-gradient-to-r from-cyan-300 to-blue-500 font-inter">
+            <div className="grid grid-cols-2 gap-4 w-screen h-[50vh] m-10 p-10">
+                <div className="text-center border-solid border-2 border-indigo-900  bg-white max-w-7xl rounded">
+                    <div className="bg-gray-700 h-2/5 flex items-center justify-center">
+                        <h5 className="text-white text-xl"> Test </h5>
+                    </div>
+                    <h5> Complete </h5>
+                </div>
+                <div className="text-center border-solid border-2 border-indigo-900  bg-white max-w-7xl rounded">
+                    <div className="bg-gray-700 h-2/5 flex items-center justify-center">
+                        <h5 className="text-white text-xl"> Test </h5>
+                    </div>
+                    <h5> Complete</h5>
+                </div>
             </div>
         </div>
     )
