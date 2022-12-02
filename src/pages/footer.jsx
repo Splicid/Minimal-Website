6e290fbfb638df43a763042e53d0a4f6e6264371
splicid <luis1abreu11@gmail.com>
2022-12-01 23:42:21 -0500

    Added footer

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -2,9 +2,19 @@
 
 const footer = () => {
     return(
-        <div className="bottom-0 w-screen h-[15vh] bg-slate-700 text-white flex items-center justify-center">
+        <div className="bottom-0 w-screen h-[25vh] bg-slate-700 text-white flex items-center justify-center">
             <div className="w-[90vw] flex flex-row text-center">
-                <div class="basis-1/3">Home</div>
+                <div class="basis-1/3">
+                    <nav> 
+                        <ul className="flex space-x-4">
+                            <li className="font-bold"><a href=""> About</a></li>
+                            <li><a href=""> About</a></li>
+                            <li><a href=""> About</a></li>
+                            <li><a href=""> About</a></li>
+                            <li><a href=""> About</a></li>
+                        </ul>
+                    </nav>
+                </div>
                 <div class="basis-1/3">About Us</div>
                 <div class="basis-1/3">Contact US</div>
             </div>
