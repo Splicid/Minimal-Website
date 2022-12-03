11cbbdf682f5ddca6e990501af1106c1121fc9e1
splicid <luis1abreu11@gmail.com>
2022-12-02 21:33:50 -0500

    Added more responsiveness for mobile

--- src/pages/title-card.jsx
+++ src/pages/title-card.jsx
@@ -10,7 +10,7 @@ const title_card = (props) => {
         <div className="grid grid-cols-2 gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
             {infos.map((Info) => (
                 <div className="flex flex-wrap flex-col border-solid border-2 border-indigo-600 lg:p-10 md:p-7 bg-white rounded-lg items-center 
-                shadow-lg shadow-indigo-900 sm:p-6" key={Info.id}>
+                shadow-lg shadow-indigo-900 sm:p-3" key={Info.id}>
                     <div className="text-center text-base">
                         <img className="inline-block" src={Logo} />
                         <div>
